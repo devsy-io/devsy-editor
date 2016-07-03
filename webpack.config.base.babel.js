5594d2746f76d3c7fad37e3702e0387d9a4ba8e4
DANIEL DE LA CRUZ CALVO <mail@dandel.es>
2016-07-03 20:04:28 +0200

    :fire: first commit

---
+++ webpack.config.base.babel.js
@@ -0,0 +1,34 @@
+import {optimize, DefinePlugin} from 'webpack'
+import {resolve} from 'path'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+
+export default {
+  entry: resolve(__dirname, 'src'),
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel-loader'],
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader'),
+      exclude: /node_modules/
+    }]
+  },
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: 'devsy-editor.js',
+    library: 'devsy-editor',
+    libraryTarget: 'umd'
+  },
+  plugins: [
+    new ExtractTextPlugin('devsy-editor.css'),
+    new optimize.OccurenceOrderPlugin(),
+    new DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ],
+  resolve: {
+    extensions: ['', '.js']
+  }
+}
---
+++ webpack.config.dev.babel.js
@@ -0,0 +1,41 @@
+import {join} from 'path'
+import {HotModuleReplacementPlugin} from 'webpack'
+
+const SRC_PATH = join(__dirname, '/src')
+const APP_PATH = join(__dirname, '/examples')
+
+export default {
+  context: APP_PATH,
+  entry: './',
+  output: {
+    path: join(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: 'http://localhost:3000/dist'
+  },
+  resolve: {
+    alias: {
+      'devsy-editor': SRC_PATH
+    },
+    extensions: ['', '.js', '.scss']
+  },
+  module: {
+    loaders: [{
+      test: /\.scss$/,
+      loaders: ['style', 'css', 'sass'],
+      include: [APP_PATH, SRC_PATH]
+    }, {
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: [APP_PATH, SRC_PATH]
+    }]
+  },
+  devServer: {
+    port: 3000,
+    stats: { colors: true },
+    inline: true,
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new HotModuleReplacementPlugin()
+  ]
+}
