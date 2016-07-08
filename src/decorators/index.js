20dcaa19ef84f1645f19eef44f621e4c301afc60
danderu <mail@dandel.es>
2016-07-08 14:50:45 +0200

    span

--- src/decorators/index.js
+++ src/decorators/index.js
@@ -2,28 +2,70 @@ import {handleStrategy, hashtagStrategy, codeStrategy} from '../strategies'
 import {HandleSpan, HashtagSpan, CodeSpan} from '../components'
 import {CompositeDecorator} from 'draft-js'
 import MultiDecorator from 'draft-js-multidecorators'
+import lowlight from 'lowlight'
+import {List} from 'immutable'
+import React from 'react'
 
 const CODE_REGEX = /`.+`/
 
 class CodeDecorator {
+  constructor(){
+    this.highlighted = {}
+  }
   getDecorations(contentBlock) {
+    const blockKey = contentBlock.getKey()
     const text = contentBlock.getText()
+    const highlights = []
+    let matches = []
+    let offset = 0
 
-    if(CODE_REGEX.test(text)){
-      let matches
-      console.log((matches = CODE_REGEX.exec(text)) !== null)
-      console.log(matches)
+    this.highlighted[blockKey] = {}
+
+    if((matches = CODE_REGEX.exec(text)) !== null) {
+      const abstractSyntaxTree = lowlight.highlight('js', text.replace(/`+/g, ''))
+      abstractSyntaxTree.value
+        .map(token => {
+          if(token.type === 'element'){
+            const tokenId = 'tok' + offset
+            const resultId = blockKey + '-' + tokenId
+            this.highlighted[blockKey][tokenId] = token
+            highlights.push(resultId)
+          }
+          else {
+            offset += token.value.length
+          }
+        })
     }
-
-    return []
+    return List(highlights)
   }
   getComponentForKey(key) {
-    debugger
-    return []
+    return (props) => {
+      props = Object.assign({}, props, {
+        className: 'prism-token token ' + props.type
+      })
+
+    return React.createElement(
+        "span",
+        props,
+        props.children
+      )
+    }
   }
   getPropsForKey(key){
-    debugger
-    return []
+    var parts = key.split('-')
+    var blockKey = parts[0]
+    var tokenId = parts[1]
+    var token = this.highlighted[blockKey][tokenId]
+
+    return {
+      type: token.type
+    }
+  }
+}
+
+function occupySlice(targetArr, start, end, componentKey) {
+  for (var ii = start; ii < end; ii++) {
+    targetArr[ii] = componentKey
   }
 }
 
