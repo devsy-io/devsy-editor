68b1745a145ef58706d230a23137b76607fb55d5
danderu <mail@dandel.es>
2016-08-15 21:57:09 +0200

    ace editor works with themes and modes. Missing a refactoring

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -1,29 +1,65 @@
 import React, {Component} from 'react'
-import onLoad from 'script-onload'
+import loadScript from '@segment/load-script'
+
+const ACE = 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.4/ace.js'
+const THEME = 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.4/theme-github.js'
+const MODE_URL = 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.4/mode-'
+
+const EXTENSION_TO_MODE = {
+  js: 'javascript',
+  json: 'json',
+  css: 'css',
+  scss: 'scss',
+  html: 'html'
+}
 
 export default class CodeEditor extends Component {
-  componentDidMount () {
-    // Load ace
-    // https://github.com/segmentio/load-script/blob/master/index.js
-    const script = window.document.createElement('script')
-    script.type = 'text/javascript'
-    script.async = true
-    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.4/ace.js'
 
-    // Initialize on load
-    onLoad(script, () => {
-      this._editor = window.ace && window.ace.edit('devsy-CodeEditor')
+  constructor(props){
+    super(props)
+
+    this.lookupFileName = (evt) => this._lookupFileName(evt)
+
+    this.state = {
+      editor: null
+    }
+  }
+
+  componentDidMount () {
+    let _editor
+
+    // Load ace
+    loadScript(ACE, (err, event) => {
+      _editor = window.ace && window.ace.edit('devsy-CodeEditor')
+
+      loadScript(THEME, (err, event) => {
+        _editor.setTheme('ace/theme/github')
+
+        this.setState({
+          editor: _editor
+        })
+      })
+    })
+  }
+
+  _lookupFileName (evt) {
+    const ext = /[^\/\\.]*$/.exec(evt.target.value)[0]
+    const mode = EXTENSION_TO_MODE[ext]
+    const _editor = this.state.editor
+    mode && loadScript(`${MODE_URL}${mode}.js`, (err, event) => {
+      const Mode = window.ace.require(`ace/mode/${mode}`).Mode
+      _editor.session.setMode(new Mode())
     })
 
-    // Attach before first script
-    const firstScript = document.getElementsByTagName('script')[0]
-    firstScript.parentNode.insertBefore(script, firstScript)
+    this.setState({
+      editor: _editor
+    })
   }
 
   render () {
     return (
       <div className='devsy-CodeEditor'>
-        <input type='text' placeholder='File name including extension...' />
+        <input type='text' placeholder='File name including extension...' onChange={this.lookupFileName} />
         <div id='devsy-CodeEditor'></div>
       </div>
     )
